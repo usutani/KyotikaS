66d5665f9eb968a29d688e5c26f71c3a540c95d6
Yasuhiro Usutani <y_usutani@mac.com>
2020-03-06 16:36:51 +0900

    KyotikaS.xcdatamodeld -> Vaults.xcdatamodeld

--- KyotikaS/ViewController.swift
+++ KyotikaS/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import CoreData
+import os.log
 
 class ViewController: UIViewController {
 
+    var viewContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var landmarks: [Landmark] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fr = NSFetchRequest<NSFetchRequestResult>(entityName: "Landmark")
+        fr.sortDescriptors = [NSSortDescriptor(key: #keyPath(Landmark.hiragana), ascending: true)]
+        do {
+            landmarks = try viewContext.fetch(fr) as! [Landmark]
+            os_log("Landmark is fetched.", log: OSLog.default, type: .info)
+        } catch {
+            os_log("Landmark is not fetched.", log: OSLog.default, type: .error)
+        }
     }
 
-
 }
-
