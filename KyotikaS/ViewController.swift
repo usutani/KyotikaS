8d3de91c387dd7d7b69e586c7049f497f918e1ff
Yasuhiro Usutani <y_usutani@mac.com>
2020-03-06 18:22:34 +0900

    JR京都駅を中心に地図を表示する。アニメーション抜き。

--- KyotikaS/ViewController.swift
+++ KyotikaS/ViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
 import CoreData
 import os.log
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    // MARK: Constants
+    static let LOC_COORD_JR_KYOTO_STATION = CLLocationCoordinate2D(latitude: 34.985, longitude: 135.758)
+    static let REGION_KYOTO = MKCoordinateRegion(center: LOC_COORD_JR_KYOTO_STATION, latitudinalMeters: 15000.0, longitudinalMeters: 15000.0)
+    
+    // MARK: Properties
+    @IBOutlet weak var mapView: MKMapView!
     var viewContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var landmarks: [Landmark] = []
 
@@ -26,6 +33,9 @@ class ViewController: UIViewController {
         } catch {
             os_log("Landmark is not fetched.", log: OSLog.default, type: .error)
         }
+        
+        // JR京都駅を中心に地図を表示する。アニメーション抜き。
+        mapView.region = ViewController.REGION_KYOTO
     }
 
 }
