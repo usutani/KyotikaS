51f5efdb70b7732361af874f6cf7e79eb22a7a2e
Yasuhiro Usutani <y_usutani@mac.com>
2020-03-20 17:25:58 +0900

    WIP 虹を表示する

--- KyotikaS/EventViewController.swift
+++ KyotikaS/EventViewController.swift
@@ -13,9 +13,11 @@ class EventViewController: UIViewController {
     var progress: Progress? = nil
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var helpLabel: UILabel!
+    var imageView: UIImageView? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        showRainbow()
         showMessage()
         showHelp()
     }
@@ -41,6 +43,49 @@ class EventViewController: UIViewController {
         DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
             self.helpLabel.isHidden = false
         }
+    }
+    
+    fileprivate func showRainbow() {
+        if progress == nil {
+            return
+        }
+        var frame = view.bounds
+        frame.origin.y += frame.size.height - frame.size.width;
+        frame.size.height = frame.size.width;
+        imageView = UIImageView(frame: frame)
+        view.insertSubview(imageView!, belowSubview: textView)
+        imageView!.layer.anchorPoint = CGPoint(x: 1.0, y: 1.0)
+        imageView!.layer.frame = frame
+        
+        imageView!.image = UIImage(named: "Rainbow")
+        
+        //TODO
+//        let animation = CAKeyframeAnimation(keyPath: "transform")
+//        let a0: CGFloat = 0.1
+//        let a1: CGFloat = CGFloat(progress!.complete)
+//        animation.values = [
+//            NSValue(caTransform3D:CATransform3DMakeScale(a0, a0, 1.0)),
+//            NSValue(caTransform3D:CATransform3DMakeScale(a1, a1, 1.0)),
+//        ]
+//        animation.keyTimes = [0.0, 1.0]
+//
+//        let alphaanimation = CAKeyframeAnimation(keyPath: "opacity")
+//        alphaanimation.values = [
+//            a0,
+//            a1,
+//        ]
+//        alphaanimation.keyTimes = animation.keyTimes
+//
+//        let theGroup = CAAnimationGroup()
+//        theGroup.animations = [
+//            animation,
+//            alphaanimation,
+//        ]
+//        theGroup.duration = 2
+//        theGroup.repeatCount = Float.infinity
+//        theGroup.autoreverses = true
+//
+//        imageView?.layer.add(theGroup, forKey: "puyopuyo")
     }
     
     @IBAction func tap(_ sender: UITapGestureRecognizer) {
